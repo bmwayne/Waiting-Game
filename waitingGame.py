2206b9c09d1ea592985a0866039cf217e724c2a0
bmwayne <rohitvarmachitti@gmail.com>
2021-01-26 19:26:57 -0600

    added clock to track start and elapsed time

--- waitingGame.py
+++ waitingGame.py
@@ -4,4 +4,10 @@ import random
 def waitinggame():
     target = random.randint(2,4)
     print("\n your target is {} seconds".format(target))
+    print(input('-------- Press Enter to begin --------'))
 
+    start = time.perf_counter()
+
+    print(input('-------- Press Enter again to stop --------'))
+
+    elapsed = time.perf_counter() - start
